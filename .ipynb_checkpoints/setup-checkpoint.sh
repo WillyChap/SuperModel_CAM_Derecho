a9e7f8190c791e9d50fb90da49f0dfe9bbe057f8
willychap <wchapman@ucsd.edu>
2023-11-02 13:35:41 -0600

    change specs

--- .ipynb_checkpoints/setup-checkpoint.sh
+++ .ipynb_checkpoints/setup-checkpoint.sh
@@ -7,9 +7,9 @@ PATH=$(echo "$PATH" | sed -e 's,/glade\/work\/wchapman\/miniconda3/[^:]\+\(:\|$\
 PATH=$(echo "$PATH" | sed -e 's,/glade\/u\/home\/wchapman\/anaconda/[^:]\+\(:\|$\),,g')
 
 module purge
-module load intel/19.0.5 ncarcompilers/0.5.0 mpt/2.22 netcdf/4.7.3 nco/4.9.5 ncl/6.6.2
-module load conda
-conda activate npl-2023b
+module load ncarenv/1.3 intel/19.0.5 ncarcompilers/0.5.0 mpt/2.22 netcdf/4.7.3 nco/4.9.5 ncl/6.6.2 python/2.7.16
+module load python/3.7.9
+ncar_pylib
 
 export CESM_ROOT=/glade/work/wchapman/cesm_supermodel/
 chmod +x init_supermodel.py 
